9db67e3a977586ed0d4fbfec2e77dd9201281abc
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-10-30 10:47:12 +0100

    Añadimos cambios al script y al index

--- scripts/index.js
+++ scripts/index.js
@@ -36,28 +36,40 @@ if(isNaN(cantidad) || cantidad <=0 ){
 function retirarBtn(){
 let retiro = parseFloat("Introduzca la cantidad a retirar")
 if(isNaN(retiro) || retiro <=0 || retiro > saldo){
-    prompt("No es posible introducir esa cantidad")
+    alert("No es posible introducir esa cantidad")
 }else{
     saldo -= retiro;
-    prompt("Cantidad retirada con éxito")
+    alert("Cantidad retirada con éxito")
     actualizarSaldoTemplate()
 }
 }
 
 function transferirBtn(){
 let monto = parseFloat("Introduzca la cantidad a transferir")
-while(){
-
-}
 if(isNaN(monto) || monto<=0 || monto>saldo){
 
 }
 }
 
 function iniciarSesion(){
-
+let pin = prompt("Introduzca su PIN para iniciar sesión")
+while(pin!==PIN_CORRECTO && intentosRestantes>1){
+    intentosRestantes -- 
+    pin = prompt(
+        `El pin introducido es incorrecto. Inténtelo de nuevo. ${intentosRestantes} intentos restantes.`
+    )
+}
+if(pin == PIN_CORRECTO){
+    alert("PIN introducido correctamente")
+}else{
+    window.location.replace()
+}
 }
 
 function actualizarSaldoTemplate(){
     saldoTemplate.innerHTML = `Su saldo es: ${saldo}€`
+}
+
+function validarIBAN(){
+    let expresionRegular = /^(ES\d{22}$/
 }
