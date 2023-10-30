4817798a7c2e68564c1c88c33577c3c9bd3372b8
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-10-30 11:59:10 +0100

    Modoficamos el script

--- scripts/index.js
+++ scripts/index.js
@@ -9,46 +9,46 @@ let saldo = 1000
 const PIN_CORRECTO = "1234";
 let intentosRestantes = 3;
 
-const depositarBtn = document.getElementById("depositar");
-const retirarBtn = document.getElementById("retirar");
-const tranferirBtn = document.getElementById("transferir");
-const saldoTemplate = document.getElementById("saldo");
-const salirBtn = document.getElementById("salir")
+const depositBtn = document.getElementById("depositar");
+const withdrawBtn = document.getElementById("retirar");
+const tranferBtn = document.getElementById("transferir");
+const money = document.getElementById("saldo");
+const exitBtn = document.getElementById("salir")
 
-depositarBtn.addEventListener("click", depositar);
-retirarBtn.addEventListener("click", retirar);
-transferirBtn.addEventListener("click", transferir);
-saldoTemplate.addEventListener("click", actualizarSaldoTemplate);
-salirBtn.addEventListener("click", () =>{
+depositBtn.addEventListener("click", depositar);
+withdrawBtn.addEventListener("click", retirar);
+transferBtn.addEventListener("click", transferir);
+money.addEventListener("click", actualizarSaldo);
+exitBtn.addEventListener("click", () =>{
     alert("Gracias por su visita. Que tenga un buen día.")
-    window.location.replace(/templates/despedida.html)
+    window.location.replace("/templates/despedida.html")
 }
 )
 
 function depositar(){
-let cantidad = parseFloat("Introduzca la cantidad a depositar")
-if(isNaN(cantidad) || cantidad <=0 ){
+const cantidadDeposito = parseFloat("Introduzca la cantidad a depositar")
+if(isNaN(cantidadDeposito) || cantidadDeposito <=0 ){
     prompt("No es posible introducir esa cantidad")
 }else{
     saldo += cantidad;
     prompt("Cantidad depositada con éxito")
-    actualizarSaldoTemplate()
+    actualizarSaldo()
 }
 }
 
 function retirar(){
-let retiro = parseFloat("Introduzca la cantidad a retirar")
-if(isNaN(retiro) || retiro <=0 || retiro > saldo){
+const cantidadRetiro = parseFloat("Introduzca la cantidad a retirar")
+if(isNaN(cantidadRetiro) || cantidadRetiro <=0 || cantidadRetiro > saldo){
     alert("No es posible introducir esa cantidad")
 }else{
     saldo -= retiro;
     alert("Cantidad retirada con éxito")
-    actualizarSaldoTemplate()
+    actualizarSaldo()
 }
 }
 
 function transferir(){
-let monto = parseFloat("Introduzca la cantidad a transferir")
+const monto = parseFloat("Introduzca la cantidad a transferir")
 if(isNaN(monto) || monto<=0 || monto>saldo){
 
 }
@@ -64,13 +64,14 @@ while(pin!==PIN_CORRECTO && intentosRestantes>1){
 }
 if(pin === PIN_CORRECTO){
     alert("PIN introducido correctamente")
-    actualizarSaldoTemplate()
+    actualizarSaldo()
 }else{
-    window.location.replace(/templates/cajeroBloqueado.html)
+    alert("No le quedan intentos restantes")
+    window.location.replace("/templates/cajeroBloqueado.html")
 }
 }
 
-function actualizarSaldoTemplate(){
+function actualizarSaldo(){
     saldoTemplate.innerHTML = `Su saldo es: ${saldo}€`
 }
 
